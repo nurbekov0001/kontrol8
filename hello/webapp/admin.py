e6fe2cb26950fcd52ef22d1554c89da7e760f546
bekbolot <nurbekovbekbolot090501@gmail.com>
2021-05-01 12:07:25 +0600

    создал модельку,создал продукты и отзывы к нему, обновил фикстуры

--- hello/webapp/admin.py
+++ hello/webapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from webapp.models import Product, Review
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'category', 'picture']
+    list_filter = ['category']
+    fields = ['name', 'description', 'category', 'picture']
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'product', 'review', 'appraisal', 'moderated']
+    fields = ['author', 'product', 'review', 'appraisal', 'moderated']
+
+
+admin.site.register(Product, ProductsAdmin)
+admin.site.register(Review, ReviewAdmin)
+
 
 # Register your models here.
--- hello/webapp/models.py
+++ hello/webapp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxLengthValidator
 
+
+CATEGORY_CHOICES = [('OTHER', 'other'), ('SAMSUNG', 'samsung'), ('ACER', 'acer'), ('IPHONE', 'iPhone')]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='название')
+    category = models.CharField(max_length=15, choices=CATEGORY_CHOICES, null=False, blank=False, default='other',
+                                verbose_name="категория товара")
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Описание товара")
+    picture = models.ImageField(null=True, blank=True, verbose_name='Каринка', upload_to='user_pics')
 # Create your models here.
+    class Meta:
+        db_table = 'Products'
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+    def __str__(self):
+        return f'{self.name}:{self.category}  '
+
+
+
+class Review (models.Model):
+    author = models.ForeignKey(get_user_model(), verbose_name="автор",
+                              on_delete=models.CASCADE, related_name='author')
+    product = models.ForeignKey('webapp.Product', related_name="product", on_delete=models.CASCADE)
+
+    review = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Отзыв')
+    appraisal = models.IntegerField(null=False, blank=False, validators=[MinValueValidator(0)],
+                                    verbose_name='отценка')
+    moderated = models.BooleanField(null=False, blank=False, verbose_name='промоделирован ли')
+
+    class Meta:
+        db_table = 'Reviews'
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'отзывы'
+
+    def __str__(self):
+        return f'{self.id}. {self.author}:{self.product}{self.review} {self.appraisal}, {self.moderated} '
+
